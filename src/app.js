5c3c03940fe88440622034b5165cd6774f4cdd36
Alex <webme.co@yandex.ru>
2023-01-08 15:48:26 +0300

    Ready for review

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
-const players = {
-  Alex: 0,
-  Dima: 100,
-  Natasha: 70,
-  Vika: 230,
+const scores = {
+  a: 20,
+  b: 100,
+  c: 70,
+  d: 230,
 };
+
+function getScore(object) {
+  let totalScore = 0;
+  for (let key in object) {
+    console.log((totalScore = totalScore + object[key]));
+  }
+}
+
+getScore(scores); // 420
